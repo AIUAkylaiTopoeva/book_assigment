7e582a3ff0e23fa59eb3a7b97652d265d448748b
AkylaiTopoeva <akylaytop@gmail.com>
2023-12-10 17:38:08 +0600

    create templates

--- blogapp/views.py
+++ blogapp/views.py
@@ -5,4 +5,12 @@ from .models import Post
 
 class PostListView(generic.ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'blogapp/home.html'
+
+class PostDetailView(generic.DetailView): 
+    model = Post
+    template_name = "blogapp/post-detail.html"
+
+class PostCreateView(generic.CreateView): 
+    template_name = "post_new.html" 
+    fields = ["title", "author", "body"]
